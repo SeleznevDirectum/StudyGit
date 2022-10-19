0049e306b22dca4b5450272f1fdd4f6ad1b7b8ef
seleznev_dv <seleznev_dv@directum.ru>
2022-10-19 17:23:01 +0300

    Создано перекрытие справочника Employee, добавлены свойства: WWID, Дата приёма, Дата увольнения, Прямой руководитель, добавлены соответсвующие контролы на форму.

---
+++ DirRX.IntegrationRubi/DirRX.IntegrationRubi.Shared/Employee/EmployeeSharedFunctions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using DirRX.IntegrationRubi.Employee;
+
+namespace DirRX.IntegrationRubi.Shared
+{
+  partial class EmployeeFunctions
+  {
+
+  }
+}
