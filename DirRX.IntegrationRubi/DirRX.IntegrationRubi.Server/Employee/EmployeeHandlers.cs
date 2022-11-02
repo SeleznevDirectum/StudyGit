e421187cbef8110abd10a551d9dffda51f5a639e
seleznev_dv <seleznev_dv@directum.ru>
2022-11-02 10:44:17 +0300

    Доработка истории сотрудника при его создании.

--- DirRX.IntegrationRubi/DirRX.IntegrationRubi.Server/Employee/EmployeeHandlers.cs
+++ DirRX.IntegrationRubi/DirRX.IntegrationRubi.Server/Employee/EmployeeHandlers.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
@@ -7,4 +7,35 @@ using DirRX.IntegrationRubi.Employee;
 
 namespace DirRX.IntegrationRubi
 {
+  partial class EmployeeServerHandlers
+  {
+
+    public override void BeforeDelete(Sungero.Domain.BeforeDeleteEventArgs e)
+    {
+      // Если у сотрудника нет подразделения, не удалять ссылку на него в подразделении(не вызываем базовое событие до удаления).
+      if (_obj.Department != null)
+        base.BeforeDelete(e);
+    }
+
+    public override void BeforeSaveHistory(Sungero.Domain.HistoryEventArgs e)
+    {
+      base.BeforeSaveHistory(e);
+      
+      var IsEmployeeCreated = e.Action == Sungero.CoreEntities.History.Action.Create;
+      var IsEmployeeUpdated = e.Action == Sungero.CoreEntities.History.Action.Update;
+      
+      // Сотрудник создан.
+      if(IsEmployeeCreated)
+      {
+        var createOperation = new Enumeration(Constants.Company.Employee.Created);
+        var createOperationDetails = createOperation;
+        
+        var comment = DirRX.IntegrationRubi.Employees.Resources.CreateHistoryCommentFormat(_obj.Person.LastName,
+          _obj.Person.FirstName, _obj.Person.MiddleName);
+        
+        e.Write(createOperation, createOperationDetails, comment);
+      }
+    }
+  }
+
 }
--- DirRX.IntegrationRubi/DirRX.IntegrationRubi.Shared/Employee/EmployeeConstants.cs
+++ DirRX.IntegrationRubi/DirRX.IntegrationRubi.Shared/Employee/EmployeeConstants.cs
@@ -1,10 +1,20 @@
-using System;
+﻿using System;
 using Sungero.Core;
 
 namespace DirRX.IntegrationRubi.Constants.Company
 {
   public static class Employee
   {
+
+    /// <summary>
+    /// Статус изменненого сотрудника.
+    /// </summary>
+    public const string Updated = "Updated";
+
+    /// <summary>
+    /// Статус созданного сотрудника.
+    /// </summary>
+    public const string Created = "Created";
 
   }
 }
