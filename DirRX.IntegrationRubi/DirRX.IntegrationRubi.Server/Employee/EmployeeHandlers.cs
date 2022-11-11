6a62e1fb52c4b90a37a1f848d564bb78e1a5fbeb
seleznev_dv <seleznev_dv@directum.ru>
2022-11-11 11:29:37 +0300

    Review: доработка истории сотруудника, запись изменения фотографии, примечания, описания и чекбоксы о получение уведомлений.

--- DirRX.IntegrationRubi/DirRX.IntegrationRubi.Server/Employee/EmployeeHandlers.cs
+++ DirRX.IntegrationRubi/DirRX.IntegrationRubi.Server/Employee/EmployeeHandlers.cs
@@ -23,41 +23,32 @@ namespace DirRX.IntegrationRubi
         // Изменилось WWID сотрудника.
         if (_obj.State.Properties.WWIDDirRX.IsChanged &&
             _obj.State.Properties.WWIDDirRX.PreviousValue != _obj.WWIDDirRX)
-        {
           commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PropertyIsChangedCommentFormat(
             Employees.Info.Properties.WWIDDirRX.LocalizedName, _obj.WWIDDirRX));
-        }
+        
         
         // Изменился логин сотрудника.
-        if (_obj.State.Properties.Login.IsChanged)
+        if (_obj.State.Properties.Login.IsChanged && !Logins.Equals(_obj.State.Properties.Login.OriginalValue, _obj.Login))
         {
           if (_obj.Login == null)
-          {
             commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PropertyIsClearedFormat(
               Employees.Info.Properties.Login.LocalizedName));
-          }
           else
-          {
             commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.EntityIsChangedCommentFormat(
               Employees.Info.Properties.Login.LocalizedName,
               _obj.Login.LoginName,
               _obj.Login.Id));
-          }
         }
         
         // Изменилась почта сотрудника.
         if (_obj.State.Properties.Email.IsChanged && _obj.State.Properties.Email.PreviousValue != _obj.Email)
         {
           if (_obj.Email == null)
-          {
             commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PropertyIsClearedFormat(
               Employees.Info.Properties.Email.LocalizedName));
-          }
           else
-          {
             commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PropertyIsChangedCommentFormat(
               Employees.Info.Properties.Email.LocalizedName, _obj.Email));
-          }
         }
         
         // Изменилось состояние сотрудника.
@@ -71,25 +62,19 @@ namespace DirRX.IntegrationRubi
         // Изменилась дата приёма сотрудника.
         if (_obj.State.Properties.StartDateDirRX.IsChanged &&
             _obj.State.Properties.StartDateDirRX.PreviousValue != _obj.StartDateDirRX)
-        {
           commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PropertyIsChangedCommentFormat(
             Employees.Info.Properties.StartDateDirRX.LocalizedName, _obj.StartDateDirRX));
-        }
         
         // Изменилась дата увольнения сотрудника.
         if (_obj.State.Properties.EndDateDirRX.IsChanged &&
             _obj.State.Properties.EndDateDirRX.PreviousValue != _obj.EndDateDirRX)
         {
           if (_obj.EndDateDirRX == null)
-          {
             commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PropertyIsClearedFormat(
               Employees.Info.Properties.EndDateDirRX.LocalizedName));
-          }
           else
-          {
             commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PropertyIsChangedCommentFormat(
               Employees.Info.Properties.EndDateDirRX.LocalizedName, _obj.EndDateDirRX));
-          }
         }
         
         // Изменился табельный номер сотрудника.
@@ -97,89 +82,119 @@ namespace DirRX.IntegrationRubi
             _obj.State.Properties.PersonnelNumber.PreviousValue != _obj.PersonnelNumber)
         {
           if (_obj.PersonnelNumber == null)
-          {
             commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PropertyIsClearedFormat(
               Employees.Info.Properties.PersonnelNumber.LocalizedName));
-          }
           else
-          {
             commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PropertyIsChangedCommentFormat(
               Employees.Info.Properties.PersonnelNumber.LocalizedName, _obj.PersonnelNumber));
-          }
         }
         
         // Изменился рабочий телефон сотрудника.
         if (_obj.State.Properties.Phone.IsChanged && _obj.State.Properties.Phone.PreviousValue != _obj.Phone)
         {
           if(_obj.Phone == null)
-          {
             commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PropertyIsClearedFormat(
               Employees.Info.Properties.Phone.LocalizedName));
-          }
           else
-          {
             commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PropertyIsChangedCommentFormat(
               Employees.Info.Properties.Phone.LocalizedName, _obj.Phone));
-          }
         }
+        
+        // Изменилось примечание у сотрудника.
+        if (_obj.State.Properties.Note.IsChanged && _obj.State.Properties.Note.PreviousValue != _obj.Note)
+        {
+          if(_obj.Note == null)
+            commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PropertyIsClearedFormat(
+              Employees.Info.Properties.Note.LocalizedName));
+          else
+            commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PropertyIsChangedCommentFormat(
+              Employees.Info.Properties.Note.LocalizedName, _obj.Note));
+        }
+        
+        // Изменилось описание у сотрудника.
+        if (_obj.State.Properties.Description.IsChanged && _obj.State.Properties.Description.PreviousValue != _obj.Description)
+        {
+          if(_obj.Description == null)
+            commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PropertyIsClearedFormat(
+              Employees.Info.Properties.Description.LocalizedName));
+          else
+            commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PropertyIsChangedCommentFormat(
+              Employees.Info.Properties.Description.LocalizedName, _obj.Description));
+        }
+        
+        // Изменилась фотография сотрудника.
+        if (_obj.State.Properties.PersonalPhoto.IsChanged)
+        {
+          if (_obj.PersonalPhoto == null)
+            commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PersonalPhotoIsCleared);
+          else
+            commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PersonalPhotoIsChanged);
+        }
+        
+        // Изменился чебокс уведомлять о просроченных заданиях.
+        if (_obj.State.Properties.NeedNotifyExpiredAssignments.IsChanged
+            && _obj.State.Properties.NeedNotifyExpiredAssignments.PreviousValue != _obj.NeedNotifyExpiredAssignments)
+          commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PropertyIsChangedCommentFormat(
+            Employees.Info.Properties.NeedNotifyExpiredAssignments.LocalizedName, _obj.NeedNotifyExpiredAssignments));
+        
+        // Изменился чебокс уведомлять о новых заданиях.
+        if (_obj.State.Properties.NeedNotifyNewAssignments.IsChanged
+            && _obj.State.Properties.NeedNotifyNewAssignments.PreviousValue != _obj.NeedNotifyNewAssignments)
+          commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PropertyIsChangedCommentFormat(
+            Employees.Info.Properties.NeedNotifyNewAssignments.LocalizedName, _obj.NeedNotifyNewAssignments));
+        
+        // Изменился чебокс уведомлять о текущих заданиях и задачах.
+        if (_obj.State.Properties.NeedNotifyAssignmentsSummary.IsChanged
+            && _obj.State.Properties.NeedNotifyAssignmentsSummary.PreviousValue != _obj.NeedNotifyAssignmentsSummary)
+          commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PropertyIsChangedCommentFormat(
+            Employees.Info.Properties.NeedNotifyAssignmentsSummary.LocalizedName, _obj.NeedNotifyAssignmentsSummary));
         
         // Изменилось ФИО.
-        if (_obj.State.Properties.Person.IsChanged)
-        {
-          {
-            commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PersonIsChangedFormat(
-              _obj.Person, _obj.Id));
-          }
-        }
+        if ((_obj.State.Properties.Person.IsChanged &&
+             !Sungero.Parties.People.Equals(_obj.State.Properties.Person.PreviousValue, _obj.Person)) ||
+            _obj.State.Properties.Name.OriginalValue != _obj.Name)
+          commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PersonIsChangedFormat(
+            _obj.Person, _obj.Id));
         
         // Изменилось подразделение сотрудника.
-        if (_obj.State.Properties.Department.IsChanged)
+        if (_obj.State.Properties.Department.IsChanged &&
+            !Sungero.Company.Departments.Equals(_obj.State.Properties.Department.PreviousValue, _obj.Department))
         {
           if (_obj.Department == null)
-          {
             commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PropertyIsClearedFormat(
               Employees.Info.Properties.Department.LocalizedName));
-          }
           else
-          {
             commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.EntityIsChangedCommentFormat(
               Employees.Info.Properties.Department.LocalizedName,
               _obj.Department,
               _obj.Department.Id));
-          }
         }
         
         // Изменилась должность сотрудника.
-        if (_obj.State.Properties.JobTitle.IsChanged)
+        if (_obj.State.Properties.JobTitle.IsChanged &&
+            !Sungero.Company.JobTitles.Equals(_obj.State.Properties.JobTitle.PreviousValue, _obj.JobTitle))
         {
           if (_obj.JobTitle == null)
-          {
             commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PropertyIsClearedFormat(
               Employees.Info.Properties.JobTitle.LocalizedName));
-          }
           else
-          {
             commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.EntityIsChangedCommentFormat(
               Employees.Info.Properties.JobTitle.LocalizedName,
               _obj.JobTitle,
               _obj.JobTitle.Id));
-          }
         }
         
         // Изменился руководитель сотрудника.
-        if (_obj.State.Properties.MangerDirRX.IsChanged)
+        if (_obj.State.Properties.MangerDirRX.IsChanged
+            && !Employees.Equals(_obj.State.Properties.MangerDirRX.PreviousValue, _obj.MangerDirRX))
         {
           if (_obj.MangerDirRX == null)
-          {
             commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.PropertyIsClearedFormat(
               Employees.Info.Properties.MangerDirRX.LocalizedName));
-          }
           else
-          {
             commentsList.Add(DirRX.IntegrationRubi.Employees.Resources.ManagerIsChangedFormat(
               _obj.MangerDirRX,
               _obj.MangerDirRX.Person.Id));
-          }
         }
         
         if (commentsList.Any())
